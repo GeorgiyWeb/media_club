c59608f4c9a6a834880239dd356dfea36c30adae
Georgiy <georgiywebdev@gmail.com>
2026-09-24 11:03:52 +0400

    2nd commit (urls.py)

---
+++ clubs/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="clubs/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    path("clubs/create/", views.club_create, name="club_create"),
+    path("clubs/vote/<int:nomination_id>/", views.vote, name="vote"),
+    path("clubs/comment/<int:pick_id>/", views.create_comment, name="create_comment"),
+    path("clubs/<int:id>/", views.club_detail, name="club_detail"),
+    path("clubs/<int:id>/join/", views.join_club, name="join_club"),
+    path("clubs/<int:id>/leave/", views.leave_club, name="leave_club"),
+    path("clubs/<int:id>/nominations/", views.nominations, name="nominations_list"),
+    path("clubs/<int:id>/nominations/create/", views.create_nomination, name="create_nomination"),
+    path("clubs/<int:id>/close/", views.close_pick, name="close_pick"),
+
+    path("profile/<str:username>/", views.profile, name="profile"),
+]
